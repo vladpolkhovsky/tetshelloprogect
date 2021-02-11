84114f2e3a1fd25f63b02fd15b523f40443687e5
vlad.polkhovsky <vlad.polkhovsky@gmail.com>
2021-02-11 13:03:10 +0300

    update 1

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -9,8 +9,8 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(HttpServletResponse.SC_OK);
-        System.out.println("Connection. + " + req.getServletPath());
+        System.out.println("Connection. + " + req.getRequestURL());
         resp.setContentType("text/html;charset=utf-8");
-        resp.getWriter().println(String.format("<h1>url = '%s'</h1>", req.getServletPath()));
+        resp.getWriter().println(String.format("<h1>url = '%s'</h1>", req.getRequestURL()));
     }
 }
