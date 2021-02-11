335a6bb5f801fe8cb5ab51dbb95771b79d5ffb7e
vlad.polkhovsky <vlad.polkhovsky@gmail.com>
2021-02-11 11:32:38 +0300

    added plugin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,73 @@
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class SocketProcessor implements Runnable {
+
+    private Socket s;
+    private InputStream is;
+    private OutputStream os;
+
+    SocketProcessor(Socket s) throws Throwable {
+        this.s = s;
+        this.is = s.getInputStream();
+        this.os = s.getOutputStream();
+    }
+
+    public void run() {
+        try {
+            readInputHeaders();
+            writeResponse("<html><body><h1>Hello from Habrahabr</h1></body></html>");
+        } catch (Throwable t) {
+            /*do nothing*/
+        } finally {
+            try {
+                s.close();
+            } catch (Throwable t) {
+                /*do nothing*/
+            }
+        }
+        System.err.println("Client processing finished");
+    }
+
+    private void writeResponse(String s) throws Throwable {
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Server: YarServer/2009-09-09\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: " + s.length() + "\r\n" +
+                "Connection: close\r\n\r\n";
+        String result = response + s;
+        os.write(result.getBytes());
+        os.flush();
+    }
+
+    private void readInputHeaders() throws Throwable {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        while(true) {
+            String s = br.readLine();
+            if(s == null || s.trim().length() == 0) {
+                break;
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(new SocketProcessor(socket)).start();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
